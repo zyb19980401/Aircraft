b4a50b1daee4031b35b77b4555601b5de4bdcda5
chriszhang <chris19980401@gmail.com>
2018-12-22 22:08:26 -0500

    Initial commit

---
+++ app/src/main/java/e/chriszhang/aircraft/StartingActivity.java
@@ -0,0 +1,13 @@
+package e.chriszhang.aircraft;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StartingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_starting);
+    }
+}
