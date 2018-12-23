7046d60bcb8f130d61d2daeeb31c1b2b4e851f23
chriszhang <chris19980401@gmail.com>
2018-12-23 00:05:52 -0500

    test push on Android Studio

--- app/src/main/java/e/chriszhang/aircraft/StartingActivity.java
+++ app/src/main/java/e/chriszhang/aircraft/StartingActivity.java
@@ -3,8 +3,11 @@ package e.chriszhang.aircraft;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class StartingActivity extends AppCompatActivity {
 
+/**
+ * test for git
+ */
+public class StartingActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
